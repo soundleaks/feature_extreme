3d1c767047ac69ca9dab443d077e9c9a06944c40
Sean Lee <iseansay@gmail.com>
2014-12-19 00:11:44 +0800

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var deploy = require('gulp-gh-pages');
 
 require('require-dir')('./gulp');
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
+});
+
+gulp.task('deploy', ['build'], function () {
+  gulp.src('./dist/**/*')
+  .pipe(deploy({}));
 });
